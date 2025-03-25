ac4d97e6a39fd7b917b77d854728da545de73d89
Francesco Daghero <francesco.daghero@polito.it>
2025-03-25 17:37:51 +0100

    update lightgbm code

---
+++ training/ensemble.py
@@ -0,0 +1,143 @@
+from cpu import set_cores
+import numpy as np
+
+SEED = 0
+np.random.seed(SEED)
+set_cores(8)
+
+
+import lightgbm as lgbm
+import optuna
+
+from preprocess import preprocess_and_window, load_parameters
+from feature_extraction import extract_features
+
+from sklearn.metrics import (
+    mean_absolute_error,
+)
+
+optuna.logging.set_verbosity(optuna.logging.WARNING)
+
+
+# A default model
+def lgbm_model(**params):
+    lgbm_params = {
+        "objective": "regression",
+        "metric": "mae",
+        "verbosity": -1,
+        "boosting_type": "gbdt",
+        "verbose": -1,
+        "seed": SEED,
+        "n_jobs": 8,
+        "deterministic": True,
+    }
+    params.update(lgbm_params)
+    return lgbm.LGBMRegressor(**params)
+
+def lgbm_optimize(trial, x, y, vx, vy):
+    """
+    Args:
+        x: input of tarining data
+        y: output of training data
+        vx: input of valid data
+        vy: output of valid data
+
+    Returns: mean abs error
+
+    """
+    lgbm_params = {
+        'learning_rate': trial.suggest_float('learning_rate', 0.0001, 0.1, log=True),
+        'max_depth': trial.suggest_int('max_depth', 3, 20),
+        'num_leaves': trial.suggest_int('num_leaves', 10, 200),
+        'reg_alpha': trial.suggest_float('reg_alpha', 1e-6, 1.0, log=True),
+        'reg_lambda': trial.suggest_float('reg_lambda', 1e-6, 1.0, log=True),
+        'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+        'min_child_samples': trial.suggest_int('min_child_samples', 5, 50),
+        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.1, 1.0),
+    }
+
+
+    model = lgbm_model(**lgbm_params)
+    model.fit(x, y, eval_set=[(vx, vy)], eval_metric="mae")
+ 
+    valid_pred = model.predict(vx)
+    mae = mean_absolute_error(vy, valid_pred)
+ 
+    return mae
+
+
+def search():
+    params = load_parameters("training/lightgbm.yaml")
+    train_samples = []
+    train_targets = []
+    valid_samples = []
+    valid_targets = []
+    test_samples = []
+    test_targets = []
+
+    for sequence_length in params.multi_split_size:
+        (
+            train_s,
+            train_t,
+            valid_s,
+            valid_t,
+            test_s,
+            test_t,
+        ) = preprocess_and_window(
+            params.data_path,
+            sequence_length,
+            params.overlap,
+            params.normalize,
+            params.raw_features,
+            params.labels,
+        )
+
+        train_s = extract_features(train_s, params.raw_features, params.feature_list)
+        valid_s = extract_features(valid_s, params.raw_features, params.feature_list)
+        test_s = extract_features(test_s, params.raw_features, params.feature_list)
+
+        train_samples.append(train_s)
+        train_targets.append(train_t)
+        valid_samples.append(valid_s)
+        valid_targets.append(valid_t)
+        test_samples.append(test_s)
+        test_targets.append(test_t)
+
+    train_samples = np.concatenate(train_samples, axis=0)
+    valid_samples = np.concatenate(valid_samples, axis=0)
+    train_targets = np.concatenate(train_targets, axis=0)
+    valid_targets = np.concatenate(valid_targets, axis=0)
+
+    # Print the sizes of the datasets
+    print(f"Train samples: {train_samples.shape}")
+    print(f"Valid samples: {valid_samples.shape}")
+    sampler = optuna.samplers.TPESampler(seed=SEED)
+    study_nasa = optuna.create_study(direction="minimize", sampler=sampler)
+    study_nasa.optimize(
+        lambda trial: lgbm_optimize(
+            trial, train_samples, train_targets, valid_samples, valid_targets
+        ),
+        n_trials=params.num_trials,
+        n_jobs=1,
+        show_progress_bar=True,
+    )
+    best_hyperparameters = study_nasa.best_params
+    best_model = lgbm_model(**best_hyperparameters)
+    best_model.fit(
+        train_samples,
+        train_targets,
+        eval_set=[(valid_samples, valid_targets)],
+        eval_names=["train", "valid"],
+        eval_metric=["mae"],
+    )
+    print(f"Best hyperparameters: {best_hyperparameters}")
+    print("Benchmarking...")
+    for idx, split_length in enumerate(params.multi_split_size):
+        test_pred = best_model.predict(test_samples[idx], verbose=0)
+        test_mae = mean_absolute_error(test_targets[idx], test_pred)
+        print(f"Test MAE - WLEN= {split_length}: {test_mae}")
+
+
+
+if __name__ == "__main__":
+    search()
--- training/feature_extraction.py
+++ training/feature_extraction.py
@@ -1,24 +1,92 @@
 import numpy as np
+from scipy.integrate import cumulative_trapezoid
 
-def extract_features(data, idx_to_name_map, feature_list):
-    RELATIVETIME = idx_to_name_map['relativeTime']
-    CURRENT = idx_to_name_map['current']
-    VOLTAGE = idx_to_name_map['voltage']
 
-    features_windows = []
+def extract_features(data, raw_features, feature_list):
+    RELATIVETIME = raw_features.index("relativeTime")
+    CURRENT = raw_features.index("current")
+    VOLTAGE = raw_features.index("voltage")
+
+    signals = {}  # Features with dimensionality that depends on the window size
+    features = {}
 
     # Fix the time to start from 0, relative to the window (n_windows, wlen, n_features)
-    time = data[:, :, RELATIVETIME] - data[:, 0, RELATIVETIME][:, np.newaxis]
-    power = data[:, :, CURRENT] * data[:, :, VOLTAGE]
-    discharge_power_rate = np.diff(power, axis=1) / np.diff(time, axis=1)
-    discharge_voltage_rate = np.diff(data[:, :, VOLTAGE], axis=1) / np.diff(time, axis=1)
-    discharge_current_rate = np.diff(data[:, :, CURRENT], axis=1) / np.diff(time, axis=1)
+    signals["relativeTime"] = (
+        data[:, :, RELATIVETIME] - data[:, 0, RELATIVETIME][:, np.newaxis]
+    )
+    signals["power"] = data[:, :, CURRENT] * data[:, :, VOLTAGE]
+    signals["resistance"] = data[:, :, VOLTAGE] / (data[:, :, CURRENT] + 1e-9)
+    signals["discharge_power_rate"] = np.diff(signals["power"], axis=1) / np.diff(
+        signals["relativeTime"], axis=1
+    )
+    signals["discharge_voltage_rate"] = np.diff(data[:, :, VOLTAGE], axis=1) / np.diff(
+        signals["relativeTime"], axis=1
+    )
+    signals["discharge_current_rate"] = np.diff(data[:, :, CURRENT], axis=1) / np.diff(
+        signals["relativeTime"], axis=1
+    )
+    signals["discharge_energy_rate"] = np.diff(signals["power"], axis=1) / np.diff(
+        signals["relativeTime"], axis=1
+    )
+    signals["discharge_temperature_rate"] = np.diff(
+        data[:, :, raw_features.index("temperature")], axis=1
+    ) / np.diff(signals["relativeTime"], axis=1)
 
-    duration = time[:, -1][:, np.newaxis]
+    signals["delta_power"] = (
+        cumulative_trapezoid(signals["power"], x=signals["relativeTime"], axis=1)
+        / 3600.0
+    )
+    signals["delta_voltage"] = (
+        cumulative_trapezoid(data[:, :, VOLTAGE], x=signals["relativeTime"], axis=1)
+        / 3600.0
+    )
+    signals["delta_current"] = (
+        cumulative_trapezoid(data[:, :, CURRENT], x=signals["relativeTime"], axis=1)
+        / 3600.0
+    )
 
-    
+    #features["duration"] = (
+    #    signals["relativeTime"][:, -1] - signals["relativeTime"][:, 0]
+    #)
+    features["wlen"] = np.ones(data.shape[0]) * data.shape[1]
 
+    for idx, col_name in enumerate(raw_features):
+        features[f"range_{col_name}"] = data[:, :, idx].max(axis=1) - data[
+            :, :, idx
+        ].min(axis=1)
+        features[f"mean_{col_name}"] = np.mean(data[:, :, idx], axis=1)
+        features[f"std_{col_name}"] = np.std(data[:, :, idx], axis=1)
+        # Bad idea, it depends on wlen
+        #features[f"sum_{col_name}"] = np.sum(data[:, :, idx], axis=1)
+        features[f"diff_{col_name}"] = data[:, :, idx][:, -1] - data[:, :, idx][:, 0]
 
+    for idx, col_name in enumerate(signals):
+        features[f"range_{col_name}"] = signals[col_name].max(axis=-1) - signals[
+            col_name
+        ].min(axis=-1)
+        features[f"mean_{col_name}"] = np.mean(signals[col_name], axis=-1)
+        features[f"std_{col_name}"] = np.std(signals[col_name], axis=-1)
+        #features[f"sum_{col_name}"] = np.sum(signals[col_name], axis=-1)
+        features[f"diff_{col_name}"] = (
+            signals[col_name][:, -1] - signals[col_name][:, 0]
+        )
 
-        
+    feature_out = []
+    for feature in feature_list:
+        if feature_list == ["all"]:
+            for f in features:
+                fval = features[f].reshape(data.shape[0], -1)
+                feature_out.append(fval)
+            break
 
+        if feature not in features and feature not in raw_features:
+            raise ValueError(f"Feature {feature} not found in the extracted features")
+        if feature in features:
+            feature_val = features[feature]
+        else:
+            feature_val = data[:, :, raw_features.index(feature)]
+
+        feature_out.append(feature_val.reshape(data.shape[0], -1))
+
+    features = np.concatenate(feature_out, axis=1)
+    return features
